55439c07d01a2fe55b9851e7fc89bdfaaf3b99ed
Leonardo R.S. Joao <leonardo.joao@ossystems.com.br>
2021-10-19 10:19:16 -0300

    Fix indentation in js file

--- js/main.js
+++ js/main.js
@@ -3,8 +3,8 @@ let context = canvas.getContext("2d");
 let box = 32;
 
 function createBackground() {
-    context.fillStyle = "#3CB371";
-    context.fillRect(0, 0, 16 * box, 16 * box);
+  context.fillStyle = "#3CB371";
+  context.fillRect(0, 0, 16 * box, 16 * box);
 }
 
 createBackground();
