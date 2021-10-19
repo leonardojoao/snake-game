0567401b046993db5646119c2e2eb6a9bc93ab45
Leonardo R.S. Joao <leonardo.joao@ossystems.com.br>
2021-10-19 17:53:25 -0300

    Create function to move snake

--- js/main.js
+++ js/main.js
@@ -2,11 +2,14 @@ let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
 let box = 32;
 let snake = [];
+let direction = "right";
 
 snake[0] ={
   x: 8 * box,
   y: 8 * box,
 }
+
+document.addEventListener("keydown", updateDirection)
 
 function createBackground() {
   context.fillStyle = "#3CB371";
@@ -20,5 +23,56 @@ function createSnake() {
   }
 }
 
-createBackground();
-createSnake();
+function moveSnake() {
+  let snakeX = snake[0].x
+  let snakeY = snake[0].y
+
+  switch(direction) {
+    case "right":
+      snakeX += box;
+      break;
+    case "left":
+      snakeX -= box;
+      break;
+    case "up":
+      snakeY -= box;
+      break;
+    case "down":
+      snakeY += box;
+      break;
+  }
+
+  snake.pop();
+
+  let newHead = {
+    x: snakeX,
+    y: snakeY,
+  }
+
+  snake.unshift(newHead);
+}
+
+function updateDirection(event) {
+  switch(true) {
+    case event.keyCode == 37 && direction != "right":
+      direction = "left";
+      break;
+    case event.keyCode == 38 && direction != "down":
+      direction = "up";
+      break;
+    case event.keyCode == 39 && direction != "left":
+      direction = "right";
+      break;
+    case event.keyCode == 40 && direction != "up":
+      direction = "down";
+      break;
+  }
+}
+
+function startGame() {
+  createBackground();
+  createSnake();
+  moveSnake();
+}
+
+let game = setInterval(startGame, 100);
